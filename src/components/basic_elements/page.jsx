072b066c12c0d4507d00bbaf57e03e1c7189b183
Dibyas98 <dibyashaktip98@gmail.com>
2024-05-23 10:15:46 +0530

    ChexkBoxes add

--- src/components/basic_elements/page.jsx
+++ src/components/basic_elements/page.jsx
@@ -10,6 +10,7 @@ import SelectFormInput from "./SelectFormInput";
 import PasswordFormInput from "./PasswordFormInput";
 import BasicInputElements from "./BasicInputElements";
 import InputSizes from "./InputSizes";
+import Checkboxes from "./Checkboxes";
 
 
 
@@ -39,118 +40,7 @@ const colors = [
 
 
 
-const Checkboxes = () => {
-    return (
-        <div className="border border-default-200 rounded-lg">
-            <div className="p-6">
-                <h4 className="card-title">Checkboxes</h4>
 
-                <div>
-                    <div className="mt-4">
-                        <div className="flex flex-col gap-2">
-                            <div className="flex items-center">
-                                <input
-                                    type="checkbox"
-                                    className="form-checkbox rounded text-primary"
-                                    id="customCheck1"
-                                    defaultChecked
-                                />
-                                <label className="ms-1.5" htmlFor="customCheck1">
-                                    Check this checkbox
-                                </label>
-                            </div>
-                            <div className="flex items-center">
-                                <input
-                                    type="checkbox"
-                                    className="form-checkbox rounded text-primary"
-                                    id="customCheck2"
-                                />
-                                <label className="ms-1.5" htmlFor="customCheck2">
-                                    Check this checkbox
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="mt-5">
-                        <h6 className="text-sm mb-2">Inline</h6>
-                        <div className="flex gap-5">
-                            <div className="flex items-center">
-                                <input
-                                    type="checkbox"
-                                    className="form-checkbox rounded text-primary"
-                                    id="InlineCheckbox1"
-                                    defaultChecked
-                                />
-                                <label className="ms-1.5" htmlFor="InlineCheckbox1">
-                                    Check this checkbox
-                                </label>
-                            </div>
-                            <div className="flex items-center">
-                                <input
-                                    type="checkbox"
-                                    className="form-checkbox rounded text-primary"
-                                    id="InlineCheckbox2"
-                                />
-                                <label className="ms-1.5" htmlFor="InlineCheckbox2">
-                                    Check this checkbox
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="mt-5">
-                        <h6 className="text-sm mb-2">Disabled</h6>
-
-                        <div className="flex gap-2">
-                            <div className="flex items-center opacity-75">
-                                <input
-                                    type="checkbox"
-                                    className="form-checkbox rounded text-primary"
-                                    id="customCheck5"
-                                    defaultChecked
-                                    disabled
-                                />
-                                <label className="ms-1.5" htmlFor="customCheck5">
-                                    Check this checkbox
-                                </label>
-                            </div>
-                            <div className="flex items-center opacity-75">
-                                <input
-                                    type="checkbox"
-                                    className="form-checkbox rounded text-primary"
-                                    id="customCheck6"
-                                    disabled
-                                />
-                                <label className="ms-1.5" htmlFor="customCheck6">
-                                    Check this checkbox
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col gap-3 mt-5">
-                        <h6>Colors</h6>
-
-                        {(colors || []).map((color, idx) => {
-                            return (
-                                <div key={idx} className="flex items-center">
-                                    <input
-                                        className={`form-checkbox rounded text-${color}`}
-                                        type="checkbox"
-                                        id={`customckeck${idx + 1}`}
-                                        defaultChecked
-                                    />
-                                    <label className="ms-1.5" htmlFor={`customckeck${idx + 1}`}>
-                                        {color.charAt(0).toUpperCase() + color.slice(1)} Checkbox
-                                    </label>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
 
 const Radios = () => {
     return (
